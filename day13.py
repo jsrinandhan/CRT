8e592a6a3d41ca61d609f02dd9deee00fa65baeb
Srinandhan <janjiralasrinandhan@gmail.com>
2025-12-10 17:24:49 +0530

    Initial commit

--- day13.py
+++ day13.py
@@ -1,4 +1,4 @@
-#'''
+'''
 l,r,a=[5,7,14,20,25],[1,8,7,9,17],[]
 m,n=len(l),len(r)
 i,j,k=0,0,0
@@ -16,4 +16,20 @@ while j<n:
     a.append(r[j])
     j+=1
 print(*a)
+#'''
+#Merge Sort
+#'''
+def merge(l,r):
+    z=[]
+    i=j=0
+    while i<len(l) and j<len(r):
+        if l[i]<r[j]:
+            z.append(l[i])
+            i+=1
+        else:
+            z.append(r[j])
+            j+=1
+    z.extend(l[i:])
+    z.extend(r[j:])
+def merge
 #'''
